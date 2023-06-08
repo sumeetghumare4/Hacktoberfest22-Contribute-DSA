d165adf05a37bda3150791880e1a3f8550ccbceb
Sumeet Ghumare <82521258+sumeetghumare4@users.noreply.github.com>
2023-06-09 02:38:12 +0530

    Add files via upload

---
+++ Source codes/Algorithms/Python/linerar_search.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+#mylist = [13,22,45,52,63,25]
+
+mylist = [10,11,12,13,14,15]
+ele = 12
+flag = 0
+for i in mylist:
+    if (i== ele):
+        print("Element found")
+        flag=1
+        break
+if(flag==0):
+        print("Element not found")
